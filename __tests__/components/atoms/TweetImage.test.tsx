2b47ea6b1bde3d987682ab8fdef1d27c4fb0f8ea
nyaomaru <nyaonyao0725@gmail.com>
2025-02-25 21:45:43 +0900

    test: update

--- __tests__/components/atoms/TweetImage.test.tsx
+++ __tests__/components/atoms/TweetImage.test.tsx
@@ -5,7 +5,7 @@ import { TweetImage } from '@/components/atoms/TweetImage'; // Adjust the path a
 
 const testSetting = {
   url: 'https://example.com',
-  src: 'https://example.com/image.jpg',
+  src: '/image.jpg',
   alt: 'Test Image',
 } as const;
 
@@ -21,7 +21,10 @@ describe('TweetImage', () => {
     render(<TweetImage url={testSetting.url} src={testSetting.src} alt={testSetting.alt} />);
 
     expect(document.querySelector('img')).toBeInTheDocument();
-    expect(document.querySelector('img')).toHaveAttribute('src', testSetting.src);
+    expect(document.querySelector('img')).toHaveAttribute(
+      'src',
+      '/_next/image?url=%2Fimage.jpg&w=384&q=75'
+    );
     expect(document.querySelector('img')).toHaveAttribute('alt', testSetting.alt);
   });
 });
